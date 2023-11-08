4a2f3daed71c13c7be5540cc58f7ba6c13e0bd11
Kayzwer <lkyu753@gmail.com>
2023-11-08 11:38:54 +0800

    fix control variate main, add q sigma

---
+++ N_Step_Q_sigma.py
@@ -0,0 +1,164 @@
+from typing import List, Optional, Tuple
+import numpy as np
+import pickle
+
+
+class Agent:
+    def __init__(self, alpha: float, gamma: float, epsilon: float,
+                 sigma: float, n_actions: int, n_step: int) -> None:
+        assert 0. < alpha <= 1.
+        assert 0. < gamma < 1.
+        assert 0. <= epsilon <= 1.
+        assert 0. <= sigma <= 1.
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.sigma = sigma
+        self.unit_m_sigma = 1. - sigma
+        self.n_actions = n_actions
+        self.n_step = n_step
+        self.state_actions_value = {}
+        self.state_memory = []
+        self.action_memory = []
+        self.reward_memory = []
+        self.state_expected_value_memory = []
+        self.greedy_policy_action_prob_memory = []
+        self.importance_sampling_ratio_memory = []
+
+    def choose_action(self, state: str, valid_actions: List[int]
+                      ) -> Tuple[int, float, float]:
+        if not state in self.state_actions_value:
+            self.state_actions_value[state] = np.zeros(
+                self.n_actions, dtype=np.float32)
+        max_ = float('-inf')
+        for valid_action in valid_actions:
+            if self.state_actions_value[state][valid_action] > max_:
+                max_ = self.state_actions_value[state][valid_action]
+        best_actions = []
+        for valid_action in valid_actions:
+            if self.state_actions_value[state][valid_action] == max_:
+                best_actions.append(valid_action)
+        action = np.random.choice(np.array(valid_actions, dtype=np.int8) if
+                                  np.random.uniform(0., 1.) <= self.epsilon
+                                  else np.array(best_actions, dtype=np.int8))
+        behaviour_action_prob = self.epsilon / len(valid_actions)
+        target_action_prob = 0.
+        best_actions_len_inv = 1. / len(best_actions)
+        if action in best_actions:
+            behaviour_action_prob += (1. - self.epsilon) * best_actions_len_inv
+            target_action_prob += best_actions_len_inv
+        return action, target_action_prob / behaviour_action_prob, target_action_prob
+
+    def store_info(self, state: Optional[str] = None,
+                   action: Optional[int] = None, reward: Optional[float] = None,
+                   state_expected_value: Optional[float] = None,
+                   greedy_policy_action_prob: Optional[float] = None,
+                   importance_sampling_ratio: Optional[float] = None) -> None:
+        if state != None:
+            if not state in self.state_actions_value:
+                self.state_actions_value[state] = np.zeros(
+                    self.n_actions, dtype=np.float32)
+            self.state_memory.append(state)
+        if action != None:
+            self.action_memory.append(action)
+        if reward != None:
+            self.reward_memory.append(reward)
+        if state_expected_value != None:
+            self.state_expected_value_memory.append(state_expected_value)
+        if greedy_policy_action_prob != None:
+            self.greedy_policy_action_prob_memory.append(
+                greedy_policy_action_prob)
+        if importance_sampling_ratio != None:
+            self.importance_sampling_ratio_memory.append(
+                importance_sampling_ratio)
+
+    def get_state_mean_value(self, state: str, valid_actions: List[int]
+                             ) -> float:
+        if len(valid_actions) == 0:
+            return 0.
+        if not state in self.state_actions_value:
+            self.state_actions_value[state] = np.zeros(
+                self.n_actions, dtype=np.float32)
+        max_ = float('-inf')
+        for valid_action in valid_actions:
+            if self.state_actions_value[state][valid_action] > max_:
+                max_ = self.state_actions_value[state][valid_action]
+        best_actions = []
+        for valid_action in valid_actions:
+            if self.state_actions_value[state][valid_action] == max_:
+                best_actions.append(valid_action)
+        mean = 0.
+        for valid_action in valid_actions:
+            weight = 0.
+            if valid_action in best_actions:
+                weight += (1. - self.epsilon) / len(best_actions)
+            weight += self.epsilon / len(valid_actions)
+            mean += weight * self.state_actions_value[state][valid_action]
+        return mean
+
+    def update(self, done: bool) -> None:
+        if len(self.state_memory) == self.n_step + 1:
+            return_sum = 0.0
+            for i in range(self.n_step - 1, -1, -1):
+                if i == self.n_step - 1:
+                    if done:
+                        return_sum += self.reward_memory[i]
+                    else:
+                        return_sum += self.state_actions_value[
+                            self.state_memory[i]][self.action_memory[i]]
+                else:
+                    return_sum = self.reward_memory[i] + self.gamma * ((
+                        self.sigma * self.importance_sampling_ratio_memory[i] +
+                        self.unit_m_sigma *
+                        self.greedy_policy_action_prob_memory[i]) * (
+                            return_sum - self.state_actions_value[
+                                self.state_memory[i]][self.action_memory[i]]) +
+                        self.state_expected_value_memory[i])
+            state = self.state_memory.pop(0)
+            action = self.action_memory.pop(0)
+            self.reward_memory.pop(0)
+            self.state_expected_value_memory.pop(0)
+            self.greedy_policy_action_prob_memory.pop(0)
+            self.importance_sampling_ratio_memory.pop(0)
+            self.state_actions_value[state][action] += self.alpha * \
+                (return_sum - self.state_actions_value[state][action])
+
+    def update_remaining_info(self) -> None:
+        while (n := len(self.state_memory)) > 1:
+            return_sum = 0.0
+            for i in range(n - 2, -1, -1):
+                if i == n - 2:
+                    return_sum += self.reward_memory[i]
+                else:
+                    return_sum = self.reward_memory[i] + self.gamma * ((
+                        self.sigma * self.importance_sampling_ratio_memory[i] +
+                        self.unit_m_sigma *
+                        self.greedy_policy_action_prob_memory[i]) * (
+                            return_sum - self.state_actions_value[
+                                self.state_memory[i]][self.action_memory[i]]) +
+                        self.state_expected_value_memory[i])
+            state = self.state_memory.pop(0)
+            action = self.action_memory.pop(0)
+            self.reward_memory.pop(0)
+            self.state_expected_value_memory.pop(0)
+            self.greedy_policy_action_prob_memory.pop(0)
+            self.importance_sampling_ratio_memory.pop(0)
+            self.state_actions_value[state][action] += self.alpha * \
+                (return_sum - self.state_actions_value[state][action])
+        self.clear_memory()
+
+    def clear_memory(self) -> None:
+        self.state_memory.clear()
+        self.action_memory.clear()
+        self.reward_memory.clear()
+        self.state_expected_value_memory.clear()
+        self.greedy_policy_action_prob_memory.clear()
+        self.importance_sampling_ratio_memory.clear()
+
+    def save(self, path: str) -> None:
+        with open(path, "wb") as f:
+            pickle.dump(self.state_actions_value, f)
+
+    def load(self, path: str) -> None:
+        with open(path, "rb") as f:
+            self.state_actions_value = pickle.load(f)
--- main_n_step_q_sigma.py
+++ main_n_step_q_sigma.py
@@ -1,10 +1,10 @@
 from TicTacToe import TicTacToe
-from N_Step_Control_Variates import Agent
+from N_Step_Q_sigma import Agent
 
 
 if __name__ == "__main__":
     env = TicTacToe()
-    agent = Agent(1., .99, .2, 9, 2)
+    agent = Agent(1., .99, .2, .5, 9, 2)
 
     state = env.reset()
     score = 0.
@@ -12,16 +12,17 @@ if __name__ == "__main__":
     agent.store_info(state=state)
     for i in range(10000000):
         while not done:
-            action, importance_sampling_ratio = agent.choose_action(
-                state, env.get_valid_actions())
+            action, importance_sampling_ratio, greedy_action_prob = \
+                agent.choose_action(state, env.get_valid_actions())
             next_state, reward, done = env.step(action)
             score += reward
             agent.store_info(
                 state=next_state, action=action, reward=reward,
                 state_expected_value=agent.get_state_mean_value(
                     next_state, env.get_valid_actions()),
+                greedy_policy_action_prob=greedy_action_prob,
                 importance_sampling_ratio=importance_sampling_ratio)
             agent.update(done)
         agent.update_remaining_info()
-        if (i + 1) % 50000 == 0:
+        if (i + 1) % 50000:
             agent.save("./ttt.pkl")
